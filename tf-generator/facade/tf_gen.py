613033a4037675175495a66411791aef471d3d45
kellycj <ckelly4283@gmail.com>
2023-09-26 10:29:54 +0100

    CA-33 Added requested changes

--- tf-generator/facade/tf_gen.py
+++ tf-generator/facade/tf_gen.py
@@ -52,7 +52,8 @@ def _generate_eks_modules(config):
                 "instance_type": group["instance_type"],
                 "name": group["name"]
             }
-            for group in config["node_groups"]}
+            for group in config["node_groups"]
+        }
 
     else:
         eks_config["fargate_profiles"] = {
@@ -104,7 +105,9 @@ def _generate_vpc_resource(config):
     :param config: Dictionary representation of  config file
     :return: Dictionary of the vpc's config options
     """
-    vpc_config = {"cidr_block": str(config["cidr_block"]) if "cidr_block" in config else DEFAULT_CIDR}
+    vpc_config = {
+        "cidr_block": str(config["cidr_block"]) if "cidr_block" in config else DEFAULT_CIDR
+    }
 
     return TFStringBuilder.generate_resource("aws_vpc", f"vpc_{config['aws_region']}", vpc_config)
 
@@ -128,34 +131,49 @@ def _generate_subnet_resources(config):
     # Calculate number of Addresses to allocate to each block (rounded to nearest power of 2)
     addresses_per_subnet = 2 ** math.floor(
         (math.log(network.num_addresses / (2 * len(availability_zones))) / math.log(2)))
+
     # Determine the minimum netmask to allocate that number of addresses
     subnet_net_mask \
         = int(32 - math.log2(addresses_per_subnet))
+
     # Difference between new masks and old masks
     num_modifiable_bits = int(subnet_net_mask - network.prefixlen)
+
     # Convert the address to an integer for calculations of subnets
     network_as_int = int(network.network_address)
 
     for i, availability_zone in enumerate(availability_zones):
         # Each AZ is assigned 2 subnets, i * 2 and i * 2 + 1
         sub_network_index = i * 2
+
         # Calculate the new base address from the index and the original address
         sub_net1_base_address = _generate_base_address(sub_network_index, network, num_modifiable_bits,
                                                        network_as_int)
+
         # Convert the new address into a string, appending the netmask
         subnet_1 = f"{ipaddress.ip_address(sub_net1_base_address)}/{subnet_net_mask}"
+
         # Create a config object for the first subnet
-        subnet1_config = {"cidr_block": subnet_1, "availability_zone": availability_zone,
-                          "vpc_id": (f"aws_vpc.vpc_{config['aws_region']}.id", "ref")}
+        subnet1_config = {
+            "cidr_block": subnet_1,
+            "availability_zone": availability_zone,
+            "vpc_id": (f"aws_vpc.vpc_{config['aws_region']}.id", "ref")
+        }
+
         # Add the first subnet to the list of subnets
         subnets.append(subnet1_config)
 
         sub_net2_base_address = _generate_base_address(sub_network_index + 1, network, num_modifiable_bits,
                                                        network_as_int)
+
         subnet_2 = f"{ipaddress.ip_address(sub_net2_base_address)}/{subnet_net_mask}"
 
-        subnet2_config = {"cidr_block": subnet_2, "availability_zone": availability_zone,
-                          "vpc_id": (f"aws_vpc.vpc_{config['aws_region']}.id", "ref")}
+        subnet2_config = {
+            "cidr_block": subnet_2,
+            "availability_zone": availability_zone,
+            "vpc_id": (f"aws_vpc.vpc_{config['aws_region']}.id", "ref")
+        }
+
         subnets.append(subnet2_config)
 
     builder = ""
@@ -166,5 +184,12 @@ def _generate_subnet_resources(config):
 
 
 def _generate_base_address(sub_network_index, network, num_modifiable_bits, network_as_int):
+    """
+    :param sub_network_index: The index of which subnet is being generated
+    :param network: The IP network object for the VPC network
+    :param num_modifiable_bits: The number of modifiable bits available for subnet generation
+    :param network_as_int: The network address of the VPC as an integer
+    :return: The base address of the new subnet
+    """
     return ((sub_network_index << (network.prefixlen - int(num_modifiable_bits))) |
             network_as_int)
