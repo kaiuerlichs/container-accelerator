a6ce3ad27de097e6536389a5978f17f4c40106a4
kellycj <ckelly4283@gmail.com>
2023-09-26 11:03:23 +0100

    CA-52 Added functionality for adding tags to generated objects

--- tf-generator/facade/tf_gen.py
+++ tf-generator/facade/tf_gen.py
@@ -1,6 +1,5 @@
 from util.tf_string_builder import TFStringBuilder
 
-
 _steps_registry = [
     "_generate_tf_header",
     "_generate_eks_modules",
@@ -35,6 +34,7 @@ def _generate_eks_modules(config):
     eks_config["cluster_version"] = str(config["eks_version"]) if "eks_version" in config else "1.27"
     eks_config["subnets"] = ("aws_subnet.private_subnet[*].id", "ref")
     eks_config["vpc_id"] = ("aws_vpc.vpc.id", "ref")
+    eks_config["tags"] = _get_tags(config)
 
     if (not config["fargate"] if "fargate" in config else True):
         eks_config["eks_managed_node_groups"] = {
@@ -44,8 +44,8 @@ def _generate_eks_modules(config):
                 "desired_capacity": group["desired_capacity"],
                 "instance_type": group["instance_type"],
                 "name": group["name"]
-            } 
-        for group in config["node_groups"]}
+            }
+            for group in config["node_groups"]}
 
     else:
         eks_config["fargate_profiles"] = {
@@ -60,18 +60,20 @@ def _generate_eks_modules(config):
 
 def _generate_k8s_namespaces(config):
     cluster_datapoint_config = {
-        "name": ("module.eks.cluster_name", "ref")
+        "name": ("module.eks.cluster_name", "ref"),
+        "tags": _get_tags(config)
     }
     k8s_provider_config = {
         "host": ("data.eks_cluster.cluster.endpoint", "ref"),
-        "cluster_ca_certificate": ("base64decode(data.eks_cluster.cluster.certificate_authority.0.data)", "ref")
+        "cluster_ca_certificate": ("base64decode(data.eks_cluster.cluster.certificate_authority.0.data)", "ref"),
+        "tags": _get_tags(config)
     }
     k8s_ns_configs = [{"metadata": {"name": ns}} for ns in config["cluster_namespaces"]]
 
     output = ""
     output += TFStringBuilder.generate_data("eks_cluster", "cluster", cluster_datapoint_config)
     output += TFStringBuilder.generate_provider("kubernetes", k8s_provider_config)
-    
+
     for ns_config in k8s_ns_configs:
         output += TFStringBuilder.generate_resource("kubernetes_namespace", ns_config["metadata"]["name"], ns_config)
 
@@ -89,3 +91,15 @@ def _generate_ingress_controller(config):
 def _generate_alb_ingress_controller(config):
     # Will be done in CA-39
     pass
+
+
+def _get_tags(config):
+    tags = {
+        "resource_owner": config["resource_owner"],
+        "environment": config["environment"] if "environment" in config else "dev",
+    }
+
+    for additional_tag in config["additional_tags"]:
+        tags.update({additional_tag["key"]: additional_tag["value"]})
+
+    return tags
