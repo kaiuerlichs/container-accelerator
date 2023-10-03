e2d74dc8ad816bdec0ec88315f8bbcd779fe7264
Kai Uerlichs <71931072+kaiuerlichs@users.noreply.github.com>
2023-10-03 15:38:21 +0100

    Fix logger name

--- deployment-validator/util/args_util.py
+++ deployment-validator/util/args_util.py
@@ -5,7 +5,7 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] (k8s-primer) %(message)s",
+    format="%(asctime)s [%(levelname)s] (deployment-validator) %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
