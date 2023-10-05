c0791d92f798113dbf68655abaf6a1bcfa8c1fb2
Kai Uerlichs <kai@uerlichs.de>
2023-10-05 19:23:00 +0100

    CA-73 Fix dpval

--- deployment-validator/app.py
+++ deployment-validator/app.py
@@ -2,12 +2,14 @@ import logging
 from util.args_util import load_args
 from facade.resource_validator import run_validation
 
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] (deployment_validator) %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
+
 
 if __name__ == "__main__":
     args = load_args()
